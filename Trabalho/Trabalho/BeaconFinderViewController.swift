89e3fff45d5fc08213e3ce755e16c4d1bb901665
caiokrodrigues <caiok.rodrigues@hotmail.com>
2015-05-07 15:28:55 -0300

    Commit

---
+++ Trabalho/Trabalho/BeaconFinderViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BeaconFinderViewController.swift
+//  Trabalho
+//
+//  Created by Caio K Rodrigues on 5/7/15.
+//  Copyright (c) 2015 ThePhodas. All rights reserved.
+//
+
+import UIKit
+
+class BeaconFinderViewController: UIViewController {
+    
+    
+    @IBOutlet weak var constraintNotification: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}

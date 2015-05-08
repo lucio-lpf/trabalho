df0ba4b533daa1baea52df236829c1083667d4ab
caiokrodrigues <caiok.rodrigues@hotmail.com>
2015-05-08 15:42:51 -0300

    Commiat e pula não deixa anal (pra rimar com ppk do mal)

---
+++ Trabalho/Trabalho/Beacon.swift
@@ -0,0 +1,15 @@
+//
+//  Beacon.swift
+//  Trabalho
+//
+//  Created by Caio K Rodrigues on 5/8/15.
+//  Copyright (c) 2015 ThePhodas. All rights reserved.
+//
+
+import CoreLocation
+
+class Beacon: CLBeacon {
+    
+    var life: NSInteger!
+    
+}

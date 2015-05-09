5b455df1a56ec5d009e55b11ff68bee9ce5d33ea
lucio-lpf <lucio.lpf@gmail.com>
2015-05-09 00:23:14 -0300

    fazendo verificações nos hits

--- Trabalho/Trabalho/BeaconStorage.swift
+++ Trabalho/Trabalho/BeaconStorage.swift
@@ -27,9 +27,30 @@ class BeaconStorage {
     func updateBeaconLifeWithId(id: NSString, newLife: NSInteger, blockSuccess: () -> Void, blockFailure: (NSError!) -> Void) {
         
         let object = PFObject(withoutDataWithClassName: "Tower", objectId: id as String)
-        object.setValue(newLife, forKey: "Life")
         
+        let restantlifes: AnyObject? =  object.valueForKey("Life")
+        // verificando se pode dar mais um hit
+        if restantlifes as! NSNumber == 0 {
+            
+            let alert = UIAlertView(title: "Impossible", message: "The tower have alredy been destroyed.", delegate: nil, cancelButtonTitle: "Ok")
+            alert.show()
+        }
+        else{
+            // verificando se é o ultimo hit
+        object.setValue(newLife, forKey: "Life")
+            if newLife == 0{
+                
+                // atribuindo o nome do last hit pro tower destroyer
+                let currentUser = PFUser.currentUser()
+                let namedestroyer: AnyObject? = currentUser?.valueForKey("username")
+                object.setValue(namedestroyer as! NSString, forKey: "destroyer")
+                
+                // da parabens pro carinha que destuiu
+                let alert = UIAlertView(title: "Congratulations!!!", message: "You have been destroyed a tower.", delegate: nil, cancelButtonTitle: "Ok")
+                alert.show()
+            }
         object.save()
+        }
 //        getBeaconWithMinor(minor, blockSuccess: { (object) -> Void in
 //            
 //            object.setValue(newLife, forKey: "Life")

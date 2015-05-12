c6945d536968a04c02e5265f958de1efec727e73
Bárbara Kudiess <bkudiess@Jay-Jay-o-Jatinho.local>
2015-05-12 12:07:14 -0300

    comitando

--- Trabalho/Trabalho/LoginViewController.swift
+++ Trabalho/Trabalho/LoginViewController.swift
@@ -27,6 +27,35 @@ class LoginViewController: UIViewController {
         navigationController!.navigationBar.shadowImage = UIImage()
         navigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self);
+    }
+    
+    
+    func keyboardWillShow(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        self.loginConstraint.constant = keyboardFrame.size.height + 20
+        self.bottomConstraint.constant = 10
+        self.topConstraint.constant = -20
+        UIView.animateWithDuration(1.0, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        var info = notification.userInfo!
+        var keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        self.loginConstraint.constant = 154
+        self.bottomConstraint.constant = 54
+        self.topConstraint.constant = 15
+        UIView.animateWithDuration(1.0, animations: { () -> Void in
+            self.view.layoutIfNeeded()
+        })
     }
 
     @IBAction func loginButton(sender: AnyObject) {
@@ -44,9 +73,9 @@ class LoginViewController: UIViewController {
         }
     }
     
-//    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-//        self.view.endEditing(true)
-//    }
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
 
     @IBAction func unwind(segue: UIStoryboardSegue) {}
     
